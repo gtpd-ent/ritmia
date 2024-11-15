6bfb3aa6b9c531ca4aca87805bade2eee8b6bf66
Gon'tzalo <112364134+diazgonza17@users.noreply.github.com>
2024-11-15 00:07:28 -0300

    fully implemented eslint config package

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import localFont from "next/font/local";
-import type { Metadata } from "next"; // eslint-disable-line no-unused-vars, @typescript-eslint/no-unused-vars
+import type { Metadata } from "next";
 import React from "react";
 import "./globals.css";
 
